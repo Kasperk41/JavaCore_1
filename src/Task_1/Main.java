22d47995a70af19cc467d65688861fc24b0b252a
ghost_joint <nikita.karmishin777@gmail.com>
2023-04-07 22:19:33 +0400

    Change Task1

--- src/Task_1/Main.java
+++ src/Task_1/Main.java
@@ -5,20 +5,20 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1,2);
-        int b = calc.minus.apply(3,1);
+        int b = calc.minus.apply(1,1);
         int c = calc.devide.apply(a, b);
 
         calc.println.accept(a);
         calc.println.accept(b);
         calc.println.accept(c);
 
-        // Код работает, но не вывод дробную часть это связано с тем,
-        // мы указывали параметр Типа Integer
-        // Если изменить код:
-        // BinaryOperator<Double> devide = (x, y) -> x / y;
-        // Consumer<Double> println = System.out::println;
-        // double c = calc.devide.apply((double) a, (double) b);
-        // то всё заработает, но другие Integer считать не будет
+        // Код не работает, ошибка на ноль делить нельзя
+        // можно выкинуть ошибку BinaryOperator<Integer> devide = (x, y) -> {
+        //        if (y != 0) return x/y;
+        //        else throw new ArithmeticException("На ноль делить нельзя");
+        //    };
+        // или оставить в качестве ответа число x
+        //BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x/ y : x;
 
     }
 }
